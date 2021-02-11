6a23f6f9483166fa3cee76d090a2ab327bbfed7d
PazAutumn <pazcontrerastello@gmail.com>
2021-02-10 22:45:17 -0300

    first commit

---
+++ src/api/services/movies.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+    protected BASE_PATH = 'https://yts.mx/api/v2';
+
+  constructor(private http: HttpClient) { }
+
+  getMovies = () => {
+    return this.http.get(`${this.BASE_PATH}/list_movies.json`).toPromise()
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
